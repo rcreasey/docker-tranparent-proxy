4ce9522021e0b64cc0bbcffc1c7ff9fec8a5c922
Ryan C. Creasey <rcreasey@blizzard.com>
2016-01-12 17:56:06 -0800

    almost there.

--- client/run.sh
+++ client/run.sh
@@ -1,8 +1,2 @@
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to 192.168.201.250:3128
-iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to 192.168.201.250:3129
-
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 3128 -w
-iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to 3129 -w
-
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to 192.168.99.103:3128 -w
-iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to 192.168.99.103:3129 -w
+iptables -t nat -A PREROUTING -p tcp -s 172.17.0.0/16 --dport 80 -j REDIRECT --to-port 3128
+iptables -t nat -A PREROUTING -p tcp -s 172.17.0.0/16 --dport 443 -j REDIRECT --to-port 3129
--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -12,14 +12,20 @@ create_cache_dir() {
   chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
 }
 
+create_ssl_db() {
+  /usr/lib64/squid/ssl_crtd -c -s ${SQUID_SSL_DIR}
+  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_SSL_DIR}
+}
+
 create_log_dir
 create_cache_dir
+create_ssl_db
 
-# allow arguments to be passed to squid3
+# allow arguments to be passed to squid
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$@"
   set --
-elif [[ ${1} == squid3 || ${1} == $(which squid3) ]]; then
+elif [[ ${1} == squid ]]; then
   EXTRA_ARGS="${@:2}"
   set --
 fi
@@ -28,10 +34,12 @@ fi
 if [[ -z ${1} ]]; then
   if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
     echo "Initializing cache..."
-    $(which squid3) -N -f /etc/squid3/squid.conf -z
+    squid -N -f /etc/squid/squid.conf -z
   fi
-  echo "Starting squid3..."
-  exec $(which squid3) -f /etc/squid3/squid.conf -NYCd 1 ${EXTRA_ARGS}
+  echo "Using this Certificate Authority:"
+  cat /etc/squid/t1.pem
+  echo "Starting squid..."
+  exec squid -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
 else
   exec "$@"
 fi
